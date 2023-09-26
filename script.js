3b26cfb0b650fb616a4df2d6edea3b77c5bb888d
Franklin <71532004+fran1014@users.noreply.github.com>
2023-09-26 17:12:25 +0200

    finalise add button function ality and looping through for each

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ async function getRandomUser() {
 
   addData(newUser);
 }
+
+//add new object to data array
+function addData(obj) {
+  data.push(obj);
+
+  updateDom();
+}
+
+//Update Dom
+
+function updateDom(providedData = data) {
+  //Clear main div
+
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+//Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
